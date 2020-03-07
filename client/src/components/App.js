dc843ed7405d8ecfac153beccd2573b793e260e6
Logan Gayler <logangayler@gmail.com>
2020-03-06 16:52:57 -0800

    commit 3

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,10 @@
 import React from "react"
-import { useCats } from "../hooks"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import Maincat from "./Maincat"
 import Listings from "./Listings"
 import "../styles/Maincat.css"
 
 export default props => {
-  const { cats } = useCats()
-  console.log(cats)
-
   return (
     <Router>
       <div className="wrapper">
