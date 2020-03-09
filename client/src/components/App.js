f5225b2f43266e5d3c84725812272fd5a255c8df
Logan Gayler <logangayler@gmail.com>
2020-03-09 16:43:58 -0700

    third commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
 import Maincat from "./Maincat"
 import Listings from "./Listings"
+import Post from "./Post"
+import CreatePost from "./CreatePost"
 import "../styles/Maincat.css"
 
 export default props => {
@@ -9,7 +12,9 @@ export default props => {
     <Router>
       <div className="wrapper">
         <Route exact path="/" component={Maincat} />
-        <Route path="/listings/:id" component={Listings} />
+        <Route exact path="/Post/:id" component={Post} />
+        <Route exact path="/:id/create" component={CreatePost} />
+        <Route exact path="/listings/:id" component={Listings} />
       </div>
     </Router>
   )
---
+++ client/src/components/CreatePost.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react"
+
+export default props => {
+  const [name, setName] = useState("")
+  const [desc, setDesc] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Post Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            palceholder="Posting Name"
+          />
+          <label>Post Description</label>
+          <textarea
+            onChange={e => setDesc(e.target.value)}
+            value={desc}
+          ></textarea>
+        </div>
+      </form>
+    </div>
+  )
+}
